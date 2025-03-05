70c920eb786376e9daf907722516ecdf5c293163
eddy <eddy.mwaniki.27@gmail.com>
2025-03-05 10:20:43 +0300

    alembic and migration

---
+++ app/migrations/versions/2fdace111404_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2fdace111404
+Revises: 
+Create Date: 2025-03-05 09:38:27.356745
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2fdace111404'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ app/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, desc, func, Column, Integer, String, CheckConstraint, DateTime
+from datetime import datetime
+
+Base = declarative_base()
+engine = create_engine("sqlite:///migrations_test.db")
+
+class Student(Base):
+    __tablename__ = "student"
+    id = Column(Integer(), primary_key = True)
+    name = Column(String())
+    email = Column(String(), unique = True)
+    grade = Column(Integer(), CheckConstraint("grade BETWEEN 1 AND 2"))
+    birthday = Column(DateTime())
+    date_enrolled = Column(DateTime(), default = datetime.now())
+    def __repr__(self):
+        return f"Student {self.id} : {self.name} : Grade {self.grade}"
